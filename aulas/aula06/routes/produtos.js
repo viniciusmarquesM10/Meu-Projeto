bc5d576b0fc70fdf371687b098155f0f4225741b
vinicius marques <viniciusmarques101204@gmail.com>
2024-03-18 22:02:52 -0300

    aula06

---
+++ aulas/aula06/routes/produtos.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const produtosController = require ('../controllers/controller_produtos')
+const router = express.Router();
+
+router.get("/", produtosController.listarTodos);
+
+router.get("/:produtoId", produtosController.buscarPeloId, produtosController.exibir);
+
+router.post("/",produtosController.criar);
+
+router.put("/:produtoId",produtosController.buscarPeloId, produtosController.atualizar);
+
+router.delete("/:produtoId",produtosController.buscarPeloId, produtosController.remover);
+
+module.exports = router;
